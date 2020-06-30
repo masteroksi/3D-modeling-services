a0f921ada532127bab844e6be7cedf54d2b1322e
masteroksi <masteroksi@gmail.com>
2020-07-01 01:02:48 +0300

    lesson18 task 6

--- Show-Time/showTime.js
+++ Show-Time/showTime.js
@@ -1,15 +1,59 @@
-function showCurrentTime(){
-    var currDate = new Date();
-    var hours = currDate.getHours();
-    var minutes = currDate.getMinutes();
-    var seconds = currDate.getSeconds();
-    if (minutes <= 9) {
-        minutes = "0" + minutes;
+function getTimeUnit(num) {
+    if (num < 10) {
+        return `0${num}`;
     }
-    if (seconds <= 9) {
-        seconds = "0" + seconds;
-    }
-    document.Clock.timer.value = hours + ":" + minutes + ":" + seconds;
-    setTimeout("showCurrentTime()", 1000);
+    return num.toString();
 }
-showCurrentTime();
+
+const DAYS = [
+    'Воскресение',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+];
+
+function init() {
+    const currDate = new Date();
+    let hours = currDate.getHours();
+    const minutes = currDate.getMinutes();
+    const seconds = currDate.getSeconds();
+    const day = currDate.getDay();
+
+    let greating = 'Добрый день';
+    if (hours < 12 && hours > 3) {
+        greating = 'Доброе утро';
+    } else if (hours > 17 && hours < 24) {
+        greating = 'Добрый вечер';
+    } else if (hours > 23 || hours < 4) {
+        greating = 'Доброй ночи';
+    }
+
+    const partOfTheDay = hours < 12 ? 'AM' : 'PM';
+    if (hours === 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours -= 12;
+    }
+
+    const nextDate = new Date(`January 1, ${currDate.getFullYear() + 1}`);
+    //Количество миллисекунд в одном дне
+    const msPerDay = 24 * 60 * 60 * 1000;
+    const newYearDaysLeft = Math.round((nextDate.getTime() - currDate.getTime()) / msPerDay);
+
+    const currentDay = `Сегодня: ${DAYS[day]}`;
+    const currentData = `Текущее время: ${getTimeUnit(hours)}:${getTimeUnit(minutes)}:${getTimeUnit(seconds)} ${partOfTheDay}`;
+    const newYearLeft = `До нового года осталось ${newYearDaysLeft} дней`;
+
+    document.getElementById('clock').innerText = greating + '\n';
+    document.getElementById('clock').innerText += currentDay + '\n';
+    document.getElementById('clock').innerText += currentData + '\n';
+    document.getElementById('clock').innerText += newYearLeft + '\n';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+});
